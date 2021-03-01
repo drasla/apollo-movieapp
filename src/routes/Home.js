2a3d7ef477fb293c85ab464d3ab5d5bab53a6a8b
Tae.Garam <drasla@naver.com>
2021-03-01 15:19:25 +0900

    #1.1 Get Movies Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,22 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import {useQuery} from "@apollo/react-hooks";
 
-export default () => "Home";
+const GET_MOVIES = gql`
+    {
+        Movies {
+            id
+            medium_cover_image
+        }
+    }
+`;
+
+export default () => {
+    const { loading, error, data } = useQuery(GET_MOVIES);
+    if(loading) {
+        return "loading...";
+    }
+    if(data && data.Movies) {
+        return data.Movies.map(m => <h1>{m.id}</h1>);
+    }
+};
